a65a5b84fea83a7e8d801a87fc18e6c706398f2f
omoredeiguma@hotmail.com <omoredeiguma@hotmail.com>
2024-08-21 16:36:58 +0000

    Update

--- models/Staging/stg_Store_location.sql
+++ models/Staging/stg_Store_location.sql
@@ -1,20 +1,20 @@
 WITH store_location_cte AS (
-    Store_id
-    City_Name
-    County
-    State_Code
-    State
-    Type
-    Latitude
-    Longitude
-    Area_Code
-    Population
-    Household_Income
-    Median_Income
-    Land_Area
-    Water_Area
+    SELECT
+    Store_id,
+    City_Name,
+    County,
+    State_Code,
+    State,
+    Type,
+    Latitude,
+    Longitude,
+    Area_Code,
+    Population,
+    Household_Income,
+    Median_Income,
+    Land_Area,
+    Water_Area,
     Time_Zone
 FROM {{ source('ecom', 'Store_Location') }}
 )
-
 select * FROM {{ source('ecom', 'Store_Location') }}
--- models/Staging/stg_sales_fact.sql
+++ models/Staging/stg_sales_fact.sql
@@ -1,17 +1,17 @@
 WITH Sales_fact_cte AS(
-    select
-    Saleschannel AS Sales_channel
-    Procureddate AS Procured_date
-    Orderdate AS Order_date
-    Shipdate AS Ship_date
-    Deliverydate AS Delivery_date
-    Salesteamid AS Salesteam_id
-    Customerid AS Customer_id
-    Storeid AS Store_id
-    Productid AS Product_id
-    Orderquantity AS Order_quantity
-    Discountedapplied AS Discounted_applied
-    Unitprice AS Unit_price
+    SELECT
+    Saleschannel AS Sales_channel,
+    Procureddate AS Procured_date,
+    Orderdate AS Order_date,
+    Shipdate AS Ship_date,
+    Deliverydate AS Delivery_date,
+    Salesteamid AS Salesteam_id,
+    Customerid AS Customer_id,
+    Storeid AS Store_id,
+    Productid AS Product_id,
+    Orderquantity AS Order_quantity,
+    Discountedapplied AS Discount_applied,
+    Unitprice AS Unit_price,
     Unitcost AS Unit_cost
 FROM {{ source('ecom', 'Sales_fact') }}
 )
