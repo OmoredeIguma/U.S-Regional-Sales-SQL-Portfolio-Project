b6764cf44c31e0c643b7055173649970dedba5e9
omoredeiguma@hotmail.com <omoredeiguma@hotmail.com>
2024-08-28 18:30:45 +0000

    Deleted the aggregates sub-folder in the model's folder.

--- models/Aggregates/Agg_Average_shipping_days.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
-SELECT 
-        AVG(DATEDIFF(DAY, Order_Date, Ship_Date)) AS Average_shipping_days
-    FROM 
-         {{ ref('stg_sales_fact') }}
